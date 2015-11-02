f058c5bf0fcc2401d3f6ef8da2c6ee4b27ad92ed
Julien Iguchi-Cartigny <kartoch@gmail.com>
2015-11-02 16:13:30 +0100

    fixing our code

--- convert.py
+++ convert.py
@@ -1,16 +1,41 @@
+#!/usr/bin/env python3
+
+import csv # Wont work if csv no present, you forgot it !
+import json # Wont work if json no present, you forgot it !
+import logging
+import sys
 import os
 
-def convert(directory):
- for filename in os.listdir(directory) :
-  if filename.endswith('.csv'):
-  	csv_filename = filename
-  	print "Opening CSV file: ",csv_filename 
-  	f=open(csv_filename, 'r')
-  	csv_reader = csv.DictReader(f)
-  	json_filename = csv_filename.split(".")[0]+".json"
-  	print "Saving JSON to file: ",json_filename
-  	jsonf = open(json_filename,'w') 
-  	data = json.dumps([r for r in csv_reader])
-  	jsonf.write(data.replace('}', ', "localisation" : "'+csv_filename.split("_")[0]+'" }'))
-  	f.close()
-  	jsonf.close()
+
+def convert(fullpath):
+    if not fullpath.endswith('.csv'):
+        # Use logging for debug statement
+        logging.info("Not a CSV file: %s", fullpath)
+        # exit if not a CSV file
+        return      
+    logging.info("Opening CSV file: %s", fullpath)
+    # pattern to open file and automatically close it
+    with open(fullpath, 'r') as f:
+        csv_reader = csv.DictReader(f)
+        json_fullpath = fullpath.split(".")[0] + ".json"
+        logging.info("Saving JSON to file: %s", json_fullpath)
+        with open(json_fullpath, 'w') as jsonf:
+            # open row by row, to be able to insert localisation field
+            for row in csv_reader:
+                row["localisation"] = os.path.basename(str(fullpath))
+                # dump one by one the entries
+                data = json.dump(row, jsonf)
+
+def main():
+    # Configure logging
+    logging.basicConfig(level=logging.INFO)
+    # Check if one paramter is present
+    assert len(sys.argv) == 2
+    # Get first parameter and pass it to the method
+    for root, dirs, files in os.walk(os.path.abspath(sys.argv[1])):
+        for f in files:
+            convert(os.path.join(root, f))
+
+# Pattern for "main" function
+if __name__ == '__main__':
+    main()
